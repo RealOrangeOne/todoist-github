38856fa223e4100c27f65c25b7339bb5ea2b0cc6
Jake Howard <git@theorangeone.net>
2020-01-11 13:23:17 +0000

    Stop caching syncs

--- todoist_github/clients.py
+++ todoist_github/clients.py
@@ -3,5 +3,5 @@ import os
 from github import Github
 from todoist import TodoistAPI
 
-todoist = TodoistAPI(os.environ["TODOIST_TOKEN"])
+todoist = TodoistAPI(os.environ["TODOIST_TOKEN"], cache=None)
 github = Github(os.environ["GITHUB_TOKEN"])
