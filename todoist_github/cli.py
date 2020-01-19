7b0a1d0f16cace8b3f5e9d0e9402b8eb25647286
Jake Howard <git@theorangeone.net>
2020-01-19 19:51:44 +0000

    Allow running script with interval

--- todoist_github/cli.py
+++ todoist_github/cli.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 
+import argparse
+import time
+
 from .tasks import ALL_TASKS
 
 
-def main():
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--interval", type=int, default=0)
+    return parser.parse_args()
+
+
+def run_tasks():
     for task in ALL_TASKS:
         print("Executing", task.__name__)
         task()
+
+
+def main():
+    args = get_args()
+    run_tasks()
+    if args.interval:
+        while True:
+            run_tasks()
+            time.sleep(args.interval)
 
 
 if __name__ == "__main__":
